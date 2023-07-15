b5e1bf35755f561d1e040a2e01290e6643acf90e
jmoddev <jmoddev@gmail.com>
2023-07-15 17:39:37 +0000

    actual maindir selection

--- grove-add.c
+++ grove-add.c
@@ -5,5 +5,24 @@
 
 int main(int argc, char* argv[])
 {
-    addpkg("linux-6.4.3.tar.xz", "", "https://cdn.kernel.org/pub/linux/kernel/v6.x/");
+    char *maindir = "";
+
+    for(int i=0; i<argc; ++i) 
+    {
+        if( !strcmp(argv[i], "-M") && argc==i+1 )
+        {
+            printf("\e[1;31merr\e[0m: -M must have a maindir as the next argument");
+            return 0;
+        }
+
+        if( !strcmp(argv[i], "-M") ) 
+        {
+            maindir = argv[i+1];
+        }
+    }
+
+    addpkg("linux-6.4.3.tar.xz", maindir, "https://cdn.kernel.org/pub/linux/kernel/v6.x/");
+    
+    return 0;
+
 }
